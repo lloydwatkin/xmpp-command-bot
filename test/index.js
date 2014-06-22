a2335de3e9479334419bc31bd1541bef330245c4
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-06-22 19:05:08 +0100

    Adding basic code

---
+++ test/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var xmpp = require('node-xmpp-server').C2SServer
+  , ltx = require('node-xmpp-core').ltx
+
+var runTests = function() {
+
+    console.log('running tests')
+    require('../index')
+    
+}
+
+var c2s = new xmpp({
+    port: 5555,
+    domain: 'localhost'
+})
+
+c2s.on('connect', function(client) {
+    c2s.on('register', function(opts, cb) {
+        cb(true)
+    })
+    client.on('authenticate', function(opts, cb) {
+        cb(null, opts)
+    })
+})
+runTests()
+
