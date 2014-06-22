84cb68782891bc607363474d2dfedaa71d2e1840
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-06-22 19:07:09 +0100

    Add a readme and example config file

---
+++ README.md
@@ -0,0 +1,34 @@
+xmpp-command-bot
+==================
+
+__Note: Not currently working, development in progress.__
+
+An XMPP bot which takes messages from a chat room or a prescribed set of users and runs commands on the local server.
+
+Build status: TBA
+
+# Setup
+
+Copy `config.example.js` to `config.js` and modify as required.
+
+## XMPP Connection
+
+TBA
+
+## Setting allowed users
+
+TBA
+
+## Connecting to a chat room
+
+TBA
+
+## Adding commands
+
+TBA
+
+# Testing 
+
+```
+npm test
+```
---
+++ config.example.js
@@ -0,0 +1,27 @@
+module.exports = {
+    connection: {
+        jid: 'lloyd@localhost',
+        password: 'password',
+        port: 5555
+    },
+    
+    commands: {
+        'uptime': {
+            command: 'uptime',
+            reply: true,
+            description: 'Find uptime of server'
+        },
+        'ls': {
+            command: 'ls',
+            arguments: true,
+            reply: true,
+            description: 'Get a directory listing'
+        },
+        'test': {
+            command: 'npm test',
+            reply: false,
+            description: 'Run the tests'
+        }   
+    }
+    
+}
