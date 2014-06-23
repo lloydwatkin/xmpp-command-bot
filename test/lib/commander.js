efd11c7b76d6b29d8423165dad40147c3ae62172
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-06-23 01:39:42 +0100

    Add commander tests to implement

---
+++ test/lib/commander.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var proxyquire = require('proxyquire')
+  , Events = require('events').EventEmitter
+  , ltx = require('node-xmpp-core').ltx
+  , should = require('should')
+
+describe('Commander', function() {
+  
+    describe('Help', function() {
+    
+        it.skip('Should return help if asked to', function() {
+            
+        })
+        
+        it.skip('Should return detailed help if available', function() {
+            
+        })
+        
+        it.skip('Should return summary if detailed message not available', function() {
+            
+        })
+        
+        it.skip('Should return error message if command does not exist', function() {
+            
+        })
+        
+    })
+    
+    describe('Commands', function() {
+        
+        it.skip('Should return error message if command not defined', function() {
+            
+        })
+        
+        it.skip('Should run command with return', function() {
+            
+        })
+        
+        it.skip('Should run command and return error', function() {
+            
+        })
+        
+        it.skip('Should run command without return', function() {
+            
+        })
+        
+        it.skip('Should reject command with bad arguments', function() {
+            
+        })
+        
+    })
+    
+})
