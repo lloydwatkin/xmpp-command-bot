8d87bada23196a025a0b5e6d2a1a249559d7b16c
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-12-24 15:06:28 +0000

    Small amount of code tidy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var configFile = './config'
-if ('testing' === process.env.NODE_ENV)
+if ('testing' === process.env.NODE_ENV) {
     configFile = './config.example'
+}
     
 var config = require(configFile)
   , Xmpp = require('./lib/xmpp')
